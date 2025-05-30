e0fd9d99ce65f2c21fded3fe3aca88a835c1766f
Laura Montes <laumoen@gmail.com>
2025-05-30 11:00:49 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the', 'our', 'my', 'your'];
+  let adj = ['small', 'big'];
+  let noun = ['object', 'pet', 'car'];
+  let domain = ['.com', '.es', '.net', '.co', '.us',];
+  
+  let first = Math.floor(Math.random() * pronoun.length);
+  let what = Math.floor(Math.random() * adj.length);
+  let who = Math.floor(Math.random() * noun.length);
+  let last = Math.floor(Math.random() * domain.length);
+
+  document.querySelector("#arrays").innerHTML = pronoun[first] + adj[what] + noun[who] + domain[last];
+  
+  console.log("arrays");
 };
